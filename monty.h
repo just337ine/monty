e7e5b990e9f602f2e7042915435fbedf74e1cded
Ugwu Justine <ugwujustineobinna@gmaiil.com>
2023-08-25 15:43:36 +0100

    create header file

---
+++ monty.h
@@ -0,0 +1,92 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/* **** Struct **** */
+extern int push_arg;
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcoode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * struct global_variable - opcoode and its function
+ * @file: the opcode
+ * @push_arg: function to handle the opcode
+ * @buffer: pointer to
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct global_variable
+{
+	FILE *file;
+	int push_arg;
+	char *buffer;
+} global_var;
+
+extern global_var global_v;
+
+typedef void (*instruct_func)(stack_t **stack, unsigned int line_number);
+
+/* **** math_function.c **** */
+void _addsub(stack_t **stack, unsigned int line_number);
+void _divmod(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
+
+/* **** convert_function.c **** */
+
+int is_integer(char *str);
+
+/**  **** free_function.c **** */
+void free_int(stack_t *head);
+
+/**  **** monty_function.c **** */
+char *parse_line(char *line, stack_t **stack, unsigned int line_number);
+instruct_func get_opcode(char *str);
+
+/**  **** print_function.c **** */
+void nothing(stack_t **stack, unsigned int line_number);
+void _pchar(stack_t **stack, unsigned int line_number);
+void _pstr(stack_t **stack, unsigned int line_number);
+void read_file(char *filename, stack_t **stack);
+
+/**  **** stack_function.c **** */
+void _push(stack_t **stack, unsigned int line_number);
+void _pall(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
+void _pint(stack_t **stack, unsigned int line_number);
+void _swap(stack_t **stack, unsigned int line_number);
+
+void _rotl(stack_t **stack, unsigned int line_number);
+void _rotr(stack_t **stack, unsigned int line_number);
+
+#endif
