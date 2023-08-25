164556116b8b50d5eb7cf77190b4e2cf456dd978
Ugwu Justine <ugwujustineobinna@gmaiil.com>
2023-08-25 15:14:11 +0100

    update free() fucntion

---
+++ free.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * free_int - Free the entire linked list.
+ * @head: Pointer to the first node of the linked list
+ */
+void free_int(stack_t *head)
+{
+	stack_t *tmp;
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
