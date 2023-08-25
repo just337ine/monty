9010836d2bc36df4d8343eda6c6d0a32a66e38be
Ugwu Justine <ugwujustineobinna@gmaiil.com>
2023-08-25 14:46:20 +0100

    update monty.c

---
+++ monty.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+global_var global_v;
+/**
+ * main - driver function for monty program
+ * @argc: number of arguments
+ * @argv: opcode file
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	stack_t *stack;
+
+	stack = NULL;
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	read_file(argv[1], &stack);
+	free_dlistint(stack);
+	return (0);
+}
